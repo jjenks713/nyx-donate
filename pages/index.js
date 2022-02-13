a520934e64dba6f5778b4f7e22fe9e22f97ee414
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2022-02-13 10:13:55 -0700

    update 775

--- pages/index.js
+++ pages/index.js
@@ -144,7 +144,7 @@ export default function Home() {
                       style={{
                         width: "250px"
                       }}
-                      value={525}
+                      value={775}
                       min={0}
                       step={400}
                       max={4000}
